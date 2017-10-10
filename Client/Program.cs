30b6c4249a66b52e17f58b56d5ad017b20a357af
Gusti <augustincalin@yahoo.com>
2017-10-10 22:44:05 +0200

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+namespace Client {
+    class Program {
+        static void Main(string[] args) {
+            Util util = new Util.UtilBuilder().UseGetLink("This comes from settings: {0}");
+            Console.WriteLine(util.GetFormatedGetLink("parameter One"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Util/Util.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities {
+    public class Util {
+        private string _linkTemplate = "This is a formated link: {0}";
+        private Util(string linkTemplate) {
+            _linkTemplate = linkTemplate;
+        }
+        public string GetFormatedGetLink(string param) {
+            return string.Format(_linkTemplate, param);
+        }
+
+        public class UtilBuilder {
+            private string _linkTemplate;
+            public UtilBuilder UseGetLink(string linkTemplate) {
+                _linkTemplate = linkTemplate;
+                return this;
+            }
+            public static implicit operator Util(UtilBuilder utilBuilder) {
+                return new Util(utilBuilder._linkTemplate);
+            }
+        }
+    }
+}
